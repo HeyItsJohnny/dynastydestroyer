89cf5aa9be635499b50abda1442c5cd48a48504d
Jonathan Laroco <jonathan.laroco@gmail.com>
2023-10-31 16:12:12 -0700

    header change

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-3xl flex justify-between items-center">
-      <Header category="Home" title="Welcome to the Dynasty Destroyer" />
+      <Header category="Dynasty Destroyer" title="Command Center" />
 
     </div>
   );
